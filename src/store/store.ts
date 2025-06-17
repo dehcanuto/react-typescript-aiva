887252c885172acaeac477c95e557479492da8bf
André Canuto <andrecanutoc@gmail.com>
2025-06-17 12:12:24 -0300

    feat: store cart done

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import favoritesReducer from './favorites';
+import cartReducer from './cart';
 
 export const store = configureStore({
   reducer: {
     favorites: favoritesReducer,
+    cart: cartReducer,
   },
 });
 
