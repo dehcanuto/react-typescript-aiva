02bf793eca2788e7fab961aaff9b36e063ea4d2e
André Canuto <andrecanutoc@gmail.com>
2025-06-18 11:48:27 -0300

    feat: lazy loading in categories

--- src/components/organisms/ProductCategories/index.tsx
+++ src/components/organisms/ProductCategories/index.tsx
@@ -10,11 +10,18 @@ import { ICategories } from '@/types/categories';
 import FlowHeader from '@/components/molecules/FlowHeader';
 
 const ProductCategories = (): JSX.Element => {
+  const [loading, setLoading] = useState<boolean>(true);
   const [categories, setCategories] = useState<ICategories[]>([]);
 
   async function getCategories() {
-    const categories = await Crud.list<ICategories[]>('categories');
-    setCategories(categories);
+    try {
+      const categories = await Crud.list<ICategories[]>('categories');
+      setCategories(categories);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
   }
 
   useEffect(() => {
@@ -25,25 +32,40 @@ const ProductCategories = (): JSX.Element => {
     <section className="mt-10 p-4 2xl:px-0 antialiased">
       <FlowHeader title="Products by category" url="/" />
       <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {categories.map((item: ICategories, index) => (
-          <Link
-            key={index}
-            href={`/category/${item.slug}`}
-            className="flex items-center rounded-lg border border-gray-200 bg-white px-4 py-2 hover:bg-gray-50 gap-4"
-          >
-            <Image
-              src={item.image}
-              width={40}
-              height={40}
-              className="size-10 rounded-full"
-              alt={item.name}
-              unoptimized
-            />
-            <span className="text-sm font-medium text-gray-900 truncate">
-              {item.name}
-            </span>
-          </Link>
-        ))}
+        {loading ? (
+          Array.from({ length: 4 }).map((_, index) => (
+            <div key={index} className="animate-pulse">
+              <div className="flex items-center rounded-lg border border-gray-200 bg-white px-4 py-2 gap-4">
+                <span className="size-10 bg-slate-200 rounded-full"></span>
+                <span className="flex w-40 h-4 bg-slate-200 rounded"></span>
+              </div>
+            </div>
+          ))
+        ) : categories.length === 0 ? (
+          <div className="col-span-full text-center text-gray-500 text-sm">
+            Nenhuma categoria encontrada.
+          </div>
+        ) : (
+          categories.map((item) => (
+            <Link
+              key={item.id}
+              href={`/category/${item.slug}`}
+              className="flex items-center rounded-lg border border-gray-200 bg-white px-4 py-2 hover:bg-gray-50 gap-4"
+            >
+              <Image
+                src={item.image}
+                width={40}
+                height={40}
+                className="size-10 rounded-full"
+                alt={item.name}
+                unoptimized
+              />
+              <span className="text-sm font-medium text-gray-900 truncate">
+                {item.name}
+              </span>
+            </Link>
+          ))
+        )}
       </div>
     </section>
   );
